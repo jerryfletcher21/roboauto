3fe7eb394f057ccf86a0230b3660f8b759273d5f
jerryfletcher21 <jerryfletcher@cock.email>
2024-06-27 19:36:38 +0200

    publish on pypi

--- setup.py
+++ setup.py
@@ -5,13 +5,19 @@
 import setuptools
 
 
+with open("README.md", "r", encoding="utf8") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
     name="roboauto",
-    version="0.3.1",
-    description="robosats cli",
-    license="ISC",
+    version="0.3.2",
     author="jerryfletcher21",
     author_email="jerryfletcher@cock.email",
+    description="robosats cli",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jerryfletcher21/roboauto",
+    license="ISC",
     packages=["roboauto"],
     scripts=["bin/roboauto"]
 )
